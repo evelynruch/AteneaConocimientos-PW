36afaecbc78b3598d2e7a67e24d8463cf906d66c
szavala <szavala@kiusys.com>
2025-11-09 01:05:35 -0300

    el add del esLINT

--- tests/registro.spec.ts
+++ tests/registro.spec.ts
@@ -13,23 +13,23 @@ let paginaLogin: PaginaLogin;
 dotenv.config();
 
 test.beforeEach(({ page }) => {
-  paginaHome = new PaginaHome(page);
-  paginaRegistro = new PaginaRegistro(page);
-  paginaLogin = new PaginaLogin(page);
-  helpers = new Helpers(page);
+    paginaHome = new PaginaHome(page);
+    paginaRegistro = new PaginaRegistro(page);
+    paginaLogin = new PaginaLogin(page);
+    helpers = new Helpers(page);
 });
 
 test('TC-3: Registro de estudiante (Sign up)', async ({ page }) => {
-  const email = `estudiante${Date.now()}@automation.com`;
-  await paginaHome.navegarAHome();
-  await paginaHome.navegarARegistro();
-  await paginaRegistro.registrarEstudiante('Juan', 'Pérez', email, 'Password123');
-  // Verificar que el registro fue exitoso
-  // Verificar que el request a /api/students/register de tipo post devuelva un 201 antes de continuar
-  await helpers.esperarPorRespuestaAPI('/api/students/register', 'POST', 201);
-  await paginaRegistro.clickButtonModalIrIniciarSesion();
-  await expect(page).toHaveURL(/.*login.*/);
-  await paginaLogin.iniciarSesion(email, 'Password123');
-  await page.waitForTimeout(10000); // Espera exactamente 10 segundos
-  await helpers.verificarTextoVisible('Hola, Juan Pérez');
+    const email = `estudiante${Date.now()}@automation.com`;
+    await paginaHome.navegarAHome();
+    await paginaHome.navegarARegistro();
+    await paginaRegistro.registrarEstudiante('Juan', 'Pérez', email, 'Password123');
+    // Verificar que el registro fue exitoso
+    // Verificar que el request a /api/students/register de tipo post devuelva un 201 antes de continuar
+    await helpers.esperarPorRespuestaAPI('/api/students/register', 'POST', 201);
+    await paginaRegistro.clickButtonModalIrIniciarSesion();
+    await expect(page).toHaveURL(/.*login.*/);
+    await paginaLogin.iniciarSesion(email, 'Password123');
+    await page.waitForTimeout(10000); // Espera exactamente 10 segundos
+    await helpers.verificarTextoVisible('Hola, Juan Pérez');
 });
