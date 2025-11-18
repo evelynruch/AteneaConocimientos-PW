d5dc751bf247f97a619180dda146a1e2ba7242a3
szavala <szavala@kiusys.com>
2025-11-17 23:16:17 -0300

    Workers y esLint.

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
     testDir: './tests',
     /* Run tests in files in parallel */
     /*fullyParallel: true,*/
-    fullyParallel: false, /* para que se ejecuten los tests en serie */
+    fullyParallel: false /* para que se ejecuten los tests en serie */,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
