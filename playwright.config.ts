792549d91b34116707f020b22c7c649885e13eaa
szavala <szavala@kiusys.com>
2025-11-17 23:15:33 -0300

    Workers y esLint.

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,14 +48,14 @@ const projects = isCI
 export default defineConfig({
     testDir: './tests',
     /* Run tests in files in parallel */
-    //fullyParallel: true,
-    fullyParallel: false,  // para que se ejecuten los tests en serie
+    /*fullyParallel: true,*/
+    fullyParallel: false, /* para que se ejecuten los tests en serie */
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    //workers: process.env.CI ? 1 : 2,
+    /* workers: process.env.CI ? 1 : 2, */
     workers: 1,
     /* Timeout for each test */
     timeout: 60000,
